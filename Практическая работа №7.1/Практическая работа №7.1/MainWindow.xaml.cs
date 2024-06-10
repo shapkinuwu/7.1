cbed8fb49dafa0b8841b6d91e9e86e66a00f51ca
shapkineee <145455550+shapkinuwu@users.noreply.github.com>
2024-06-10 21:23:42 +0300

    Add files via upload

---
+++ Практическая работа №7.1/Практическая работа №7.1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Практическая_работа__7._1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Timer timer; // Объявление таймера
+
+        public MainWindow()
+        {
+
+            timer = new Timer();
+            timer.Interval = 5000; // Устанавливаем интервал срабатывания таймера (в миллисекундах)
+            timer.Elapsed += Timer_Elapsed; // Добавляем обработчик события срабатывания таймера
+            timer.Start(); // Запускаем таймер
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                MessageBox.Show("Рецепт оказался опасным для жизни"); // Выводим сообщение при срабатывании таймера
+            });
+        }
+    }
+}
